9b260a5a2bce755e5119651a2fadaea867d4e981
MethilaRaya <methilaraya@gmail.com>
2023-09-12 14:02:45 +0600

    certificate added

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -18,9 +18,9 @@ const Awards = () => {
                 
                 <div className='mt-4 '>
                     <p className='text-text-[#f9fafc] font-medium'>1. Leveraging ICT for Employment and Growth of the IT-ITES Industry (LICT-NUS). </p>  
-                    {/* <div className="mt-2 ml-4">
-                        <a className="underline font-bold" href="https://github.com/MethilaRaya/fb_data_scrapping" rel="noreferrer" target="_blank">Certificate ⮕</a>
-                    </div>                  */}
+                    <div className="mt-2 ml-4">
+                        <a className="underline font-bold" href="https://drive.google.com/file/d/13AO55116TZSOjn1cjdm6-dFQQ31IGJ8w/view?usp=sharing" rel="noreferrer" target="_blank">Certificate ⮕</a>
+                    </div>                 
                 </div>
                 <div className='mt-4 '>
                     <p className='text-text-[#f9fafc] font-medium'>2. Participated in 2<sup>nd</sup> Bangladesh Electronics Olympiad 2017.</p>   
